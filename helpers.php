fc35c06136ca67299ea6a2bf4e1bcd07da63d9e3
Alexander Zaitsev <azaitsev@ed3p.com>
2023-02-02 19:03:21 +0200

    clear code

--- helpers.php
+++ helpers.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * Перевіряє передану дату на відповідність формату 'ГГГГ-ММ-ДД'
- *
- * Приклади використання:
- * isDateValid('2019-01-01'); // true
- * isDateValid('2016-02-29'); // true
- * isDateValid('2019-04-31'); // false
- * isDateValid('10.10.2010'); // false
- * isDateValid('10/10/2010'); // false
- *
- * @param string $date Дата у вигляді рядка
- *
- * @return bool true у разі збігу з форматом 'ГГГГ-ММ-ДД', інакше false
- */
 function isDateValid(string $date) : bool {
     $format_to_check = 'Y-m-d';
     $dateTimeObj = date_create_from_format($format_to_check, $date);
@@ -20,20 +6,11 @@ function isDateValid(string $date) : bool {
     return $dateTimeObj !== false && array_sum(date_get_last_errors()) === 0;
 }
 
-/**
- * Створює підготовлений вираз на основі готового SQL запиту і переданих даних
- *
- * @param $link mysqli Ресурс з'єднання
- * @param $sql string SQL запит із плейсхолдерами замість значень
- * @param array $data Дані для вставки на місце плейсхолдерів
- *
- * @return mysqli_stmt Підготовлений вираз
- */
 function dbGetPrepareStmt($link, $sql, $data = []) {
     $stmt = mysqli_prepare($link, $sql);
 
     if ($stmt === false) {
-        $errorMsg = 'Не вдалося ініціалізувати підготовлений вираз: ' . mysqli_error($link);
+        $errorMsg = 'пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ: ' . mysqli_error($link);
         throw new ErrorException($errorMsg);
     }
 
@@ -66,7 +43,7 @@ function dbGetPrepareStmt($link, $sql, $data = []) {
         $func(...$values);
 
         if (mysqli_errno($link) > 0) {
-            $errorMsg = 'Не вдалося пов\'язати підготовлений вираз із параметрами: ' . mysqli_error($link);
+            $errorMsg = 'пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ\'пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: ' . mysqli_error($link);
             throw new ErrorException($errorMsg);
         }
     }
@@ -74,28 +51,6 @@ function dbGetPrepareStmt($link, $sql, $data = []) {
     return $stmt;
 }
 
-/**
- * Повертає коректну форму множини
- * Обмеження: тільки для цілих чисел
- *
- * Приклад використання:
- * $remainingMinutes = 5;
- * echo "Я поставив таймер на {$remainingMinutes} " .
- *     getNounPluralForm(
- *         $remainingMinutes,
- *         'хвилина',
- *         'хвилини',
- *         'хвилин'
- *     );
- * Результат: "Я поставив таймер на 5 хвилин"
- *
- * @param int $number Число, за яким обчислюємо форму множини
- * @param string $one Форма однини: яблуко, година, хвилина
- * @param string $two Форма множини для 2, 3, 4: яблука, години, хвилини
- * @param string $many Форма множини для решти чисел
- *
- * @return string Розрахована форма множини
- */
 function getNounPluralForm (int $number, string $one, string $two, string $many): string
 {
     $number = (int) $number;
@@ -120,13 +75,6 @@ function getNounPluralForm (int $number, string $one, string $two, string $many)
     }
 }
 
-/**
- * Підключає шаблон, передає туди дані і повертає підсумковий HTML контент
- *
- * @param string $name Шлях до файлу шаблону відносно папки templates
- * @param array $data Асоціативний масив із даними для шаблону
- * @return string Підсумковий HTML
- */
 function renderTemplate($name, array $data = []) {
     $name = 'templates/' . $name;
     $result = '';
--- templates/a.php
+++
@@ -1,9 +0,0 @@
-
-
-<div>
-	user: <?= $lname; ?>
-	<img class="avatar-photo" src="<?=$src?>">
-	<div><?= $fname; ?> <?=$lname; ?> <?=$page; ?></div>
-	<?= $foto?>
-	<hr>
-</div>
